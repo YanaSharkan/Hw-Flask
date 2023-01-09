8979e72047505c44e5a642bf4458201d1e787428
Yana <yana.sh93@gmail.com>
2023-01-09 22:04:46 +0200

    Implement all endpoints

---
+++ README.md
@@ -0,0 +1,15 @@
+## Tracks catalog application
+
+Tracks catalog application that provides statistic of track.
+
+###### Implemented functions:
+* get_names()
+  (Return the count of unique artist).
+* get_number_of_records()
+  (Output total number of records).
+* get_tracks_by_genre()
+  (Output tracks by selected genre).
+* get_tracks_with_duration()
+  (Output the tracks with duration).
+* get_tracks_statistics()
+  (Output average duration of track and total duration of all tracks).
--- flaskr/tracks.py
+++ flaskr/tracks.py
@@ -21,10 +21,38 @@ def get_names():
 @bp.route('/tracks')
 def get_number_of_records():
     db = get_db()
-    records = db.execute(
+    record = db.execute(
         'SELECT count(*) AS `count`'
         'FROM tracks'
     ).fetchone()
-    return render_template('tracks/tracks_count.html', data=records)
+    return render_template('tracks/tracks_count.html', data=record)
 
 
+@bp.route('/tracks/<genre>')
+def get_tracks_by_genre(genre):
+    db = get_db()
+    record = db.execute(
+        'SELECT count(*) AS `count`, genre '
+        'FROM tracks WHERE genre = ?',
+        (genre,)
+    ).fetchone()
+    return render_template('tracks/tracks_by_genre.html', data=record)
+
+
+@bp.route('/tracks-sec')
+def get_tracks_with_duration():
+    db = get_db()
+    records = db.execute(
+        'SELECT title, `length` FROM tracks'
+    ).fetchall()
+    return render_template('tracks/tracks_with_duration.html', data=records)
+
+
+@bp.route('/tracks-sec/statistics')
+def get_tracks_statistics():
+    db = get_db()
+    record = db.execute(
+        'SELECT SUM(length) AS total, AVG(length) AS average '
+        'FROM tracks'
+    ).fetchone()
+    return render_template('tracks/tracks_statistics.html', data=record)
